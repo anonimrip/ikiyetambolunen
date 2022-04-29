ed61071f57758730b25e75f3602a4cdc99f1ca25
EREN FELEKOGLU <EREN FELEKOGLU@BIL-12>
2022-04-29 10:47:22 +0300

    Proje dosyası ekle.

---
+++ ikiyetambölünen/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace nothesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] dize1 = new int[10];
+            for (int i = 0; i < dize1.Length; i++)
+            {
+                Console.WriteLine(i + ". Yaşını Giriniz: ");
+                dize1[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            for (int c = 0; c < dize1.Length; c++)
+            {
+                if (dize1[c] % 2 == 0)
+                {
+                    Console.WriteLine(dize1[c]);
+                }
+            }
+        }
+    }
+}
